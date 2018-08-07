e9109e5336f6e156e30463066280082283158c26
xzy <xzy.vip@qq.com>
2018-08-07 16:05:11 +0800

    Editor

--- resources/views/layouts/app.blade.php
+++ resources/views/layouts/app.blade.php
@@ -12,6 +12,8 @@
 
     <!--  Styles  -->
     <link href="{{ asset('css/app.css') }}" rel="stylesheet">
+    @yield('styles')
+
 </head>
 <body>
     <div id="app" class="{{ route_class() }}-page">
@@ -30,6 +32,7 @@
 
     <!-- Scripts  -->
     <script type="text/javascript" src="{{ asset('js/app.js') }}"></script>
+    @yield('scripts')
 </body>
 
 </html>
