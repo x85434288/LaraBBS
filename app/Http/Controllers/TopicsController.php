c846239d220f857fc3efea28e4f25d3e646e8147
xzy <xzy.vip@qq.com>
2018-08-10 10:53:57 +0800

    Add queue for translate slug

--- app/Http/Controllers/TopicsController.php
+++ app/Http/Controllers/TopicsController.php
@@ -25,8 +25,13 @@ class TopicsController extends Controller
 
 	}
 
-    public function show(Topic $topic)
+    public function show(Topic $topic, Request $request)
     {
+
+		if( ! empty($topic->slug) && $topic->slug != $request->slug){
+
+			return redirect($topic->link(), 301);
+		}
 
         return view('topics.show', compact('topic'));
     }
--- app/Observers/TopicObserver.php
+++ app/Observers/TopicObserver.php
@@ -4,6 +4,7 @@ namespace App\Observers;
 
 use App\Models\Topic;
 use App\Handlers\SlugTranslateHandler;
+use App\Jobs\TranslateSlug;
 
 // creating, created, updating, updated, saving,
 // saved,  deleting, deleted, restoring, restored
@@ -14,12 +15,29 @@ class TopicObserver
     public function saving(Topic $topic)
     {
 
+        // XSS文件过滤
         $topic->body = clean($topic->body, 'user_topic_body');
+
+        //生成话题摘录
         $topic->excerpt = make_excerpt($topic->body);
 
+
+
+
+    }
+
+
+    public function saved(Topic $topic)
+    {
+
+        // 如 slug 字段无内容，即使用翻译器对 title 进行翻译
         if(!$topic->slug){
 
-            $topic->slug = app(SlugTranslateHandler::class)->translate($topic->title);
+            //$topic->slug = app(SlugTranslateHandler::class)->translate($topic->title);
+            //调用队列执行
+
+            dispatch(new TranslateSlug($topic));
+
         }
 
     }
