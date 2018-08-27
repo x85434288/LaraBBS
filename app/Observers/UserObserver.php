0cdef80c0667733f1330f979258d228f970ba38c
xzy <xzy.vip@qq.com>
2018-08-27 18:27:44 +0800

    User default avatar after signup

--- app/Observers/UserObserver.php
+++ app/Observers/UserObserver.php
@@ -12,12 +12,22 @@ class UserObserver
     public function creating(User $user)
     {
         //
-        $user->avatar = "";
+
 
     }
 
     public function updating(User $user)
     {
         //
+    }
+
+    public function saving(User $user)
+    {
+        //当头像为空时用默认头像代替
+        if(empty($user->avatar)){
+            $user->avatar = "https://fsdhubcdn.phphub.org/uploads/images/201710/30/1/TrJS40Ey5k.png";
+        }
+
+
     }
 }
